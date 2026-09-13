5aa1b86c005b511dc07cf5ec4171585fbfa79195
hapo3005 <jan.nikolic@web.de>
2026-09-13 10:51:27 +0200

    chore: configure Astro for GitHub Pages

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://hapo3005.github.io',
+  base: '/weinkommission',
+  output: 'static',
+  trailingSlash: 'always'
+});
